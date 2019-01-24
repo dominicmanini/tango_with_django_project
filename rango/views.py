5b5cb904fafada341fa1327abc22c5f44a2a95dd
dominicmanini <2310536m@student.gla.ac.uk>
2019-01-24 16:56:21 +0000

    4th chapter commit

--- rango/views.py
+++ rango/views.py
@@ -3,7 +3,11 @@ from django.shortcuts import render
 
 from django.http import HttpResponse
 def index(request):
+    context_dict = {"boldmessage": "Crunch, creamy, cookie, candy, cupcake!"}
+    return render(request, "rango/index.html", context = context_dict)
     return HttpResponse("Rango says hey there partner! <br /> <a href = '/rango/about'>About</a>")
 
 def about(request):
+    context_dict = {"boldmessage": "Crunch, creamy, cookie, candy, cupcake!"}
+    return render(request, "rango/about.html", context = context_dict)
     return HttpResponse("Rango says here is the about page. <br /> <a href = '/rango/'>Index</a>")
--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -14,7 +14,9 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
+TEMPLATE_DIR = os.path.join(BASE_DIR, "templates")
+STATIC_DIR = os.path.join(BASE_DIR, "static")
+MEDIA_DIR = os.path.join(BASE_DIR, 'media')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
@@ -55,7 +57,7 @@ ROOT_URLCONF = 'tango_with_django_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATE_DIR,],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -63,6 +65,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.media',
             ],
         },
     },
@@ -100,6 +103,12 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+# Static
+STATICFILES_DIRS = [STATIC_DIR, ]
+
+#Media
+MEDIA_ROOT = MEDIA_DIR
+MEDIA_URL = "/media/"
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.11/topics/i18n/
