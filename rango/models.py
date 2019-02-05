91b005aeb6fd61177a7d6db6b6f02fd06c849e7c
dominicmanini <231056m@student.gla.ac.uk>
2019-02-05 16:09:08 +0000

    Chapter 6 commit up to exercise

--- rango/models.py
+++ rango/models.py
@@ -1,12 +1,13 @@
 from django.db import models
-from django.template.defaultfilters import slugify
+from django.template.defaultfilters import slugify
+
 
 # Create your models here.
 class Category(models.Model):
     name = models.CharField(max_length=128, unique=True)
     views = models.IntegerField(default=0)
     likes = models.IntegerField(default=0)
-    slug = models.SlugField()
+    slug = models.SlugField(unique=True)
 
     def save(self, *args, **kwargs):
         self.slug = slugify(self.name)
@@ -24,4 +25,5 @@ class Page(models.Model):
     url = models.URLField()
     views = models.IntegerField(default=0)
     def __str__(self): # For Python 2, use __unicode__ too
-        return self.title
+        return self.title
+
